f743e75b9b6406866785777f70e1ff0b8510c649
Andres Felipe Villarraga Arango <avillarraga@opc.com.co>
2023-03-21 12:02:52 -0500

    Se avanza con la generacion del pdf

--- components/pdf/controller.js
+++ components/pdf/controller.js
@@ -5,43 +5,21 @@ const pdf     = require('puppeteer')
 const hbs     = require('handlebars')
 
 
-
-
-/* Configuracion */
-const options = {
-    formate: 'A4',
-        orientation: 'portrait',
-        border: '2mm',
-        childProcessOptions: {
-            env: {
-              OPENSSL_CONF: '/dev/null',
-            },
-          }
-}
-
-
-const viewBill = async (data) =>{
-    //const filePath = path.join(process.cwd(),'templates','bill.hbs')
-    console.log(data)
-    const html = fs.readFileSync(path.join(__dirname,'../../templates/bill.hbs'),'utf-8')
-const info = {
-    products: data
-}
-    return hbs.compile(html)(info)
-}
-
 const pdfBill = async ({ product }) => {
-   // const html = fs.readFileSync(path.join(__dirname,'../../view/templatesd/templete.html'),'utf-8')
-   const html = ''
+    //Name del pdf
     const pdfName = Math.random() + '_doc.pdf'
+    
+    //productos 
     const table = []
 
+    //Calcula el valor total de cualquier propiedad numerica
     const totalValue = (obj, key = 'total') => {
         let total = 0
         obj.forEach(f => total += parseFloat(f[`${key}`].toString()))
         return total.toFixed(2)
     }
     
+    //Se crea el objeto de los productos dinamicos
     product.forEach(d =>{
         const total = ( parseInt(d.quantity) * parseFloat(d.unit_Price) ) - parseFloat(d.discount == '' ? '0': d.discount) 
         table.push({       
@@ -55,54 +33,53 @@ const pdfBill = async ({ product }) => {
         })  
     })
 
-    //iva 
+
+    //Calculos necesarios para la factura 
     const subtotal = totalValue(table)
     const tax = (subtotal * 12) / 100
     const totalTax = (parseFloat(subtotal) + parseFloat(tax))
 
 
-    /* html dinamico */
-    const obj = {
-        prodlist : table,
-        subtotal,
-        iva      : tax.toFixed(2),
-        total    : totalTax.toFixed(2)
+
+    //Informacion para generacion del pdf
+    const pdf = {
+        product:table,
+        pdfName:pdfName,
+        path:`./docs/${pdfName}`,
+        template:'bill'
     }
 
+    await generateInvoiceHtml(pdf)
 
-
-    const document = {
-        html,
-        data:{
-            products:obj
-        },
-        path:`./docs/${pdfName}`
-    }
-
-
-
-    const browser = await pdf.launch()
-    const newPdf  = await browser.newPage()
-
-
-    const content = await viewBill(table)
-
-
-    await newPdf.setContent(content)
-    await newPdf.emulateMediaType('screen')
-    await newPdf.pdf({
-        path:document.path,
-        format:'A4',
-        printBackground:true
-    })
-    
-    console.log('done')
-    await browser.close()
 
 }
 
+const htmlView = (template,data) =>{
+    const html = fs.readFileSync(path.join(__dirname,`../../templates/${template}.hbs`),'utf-8')
+    const info = {  products: data }
+    return hbs.compile(html)(info)
+}
+
+const generateInvoiceHtml = async ({pdfName, product,template,path}) =>{
+    console.log(`generating pdf: ${pdfName} ...`)
+    const browser = await pdf.launch()
+    const page  = await browser.newPage()
+
+    //PDF HTML VIEW 
+    await page.setContent(htmlView(template,product))
+
+    //CONFIG
+    await page.emulateMediaType('screen')
+    await page.pdf({
+        path:path,
+        format:'A4',
+        printBackground:true
+    })
 
 
+    console.log('done')
+    await browser.close()
+}
 
 module.exports = {
     pdfBill
