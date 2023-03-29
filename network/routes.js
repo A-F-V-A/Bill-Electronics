a565b5ce3c79851fe7f09610dd2ff23d75291871
A-F-V-A <andresf.villarragaa@gmail.com>
2023-03-29 13:14:06 -0500

    Se crea ruta para new login

--- network/routes.js
+++ network/routes.js
@@ -1,10 +1,12 @@
 // Modulos
 const express = require('express')
 const bill    = require('../components/bill/network')
+const login    = require('../components/login/controller')
 
 //Logic
 const routes = server =>{
     server.use('/xmlGenereteBill',bill)
+    server.use('/login',login)
 }
 
 module.exports = routes
