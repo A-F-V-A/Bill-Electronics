ed54fa1ddc244f07ba02475936ed3ee2fe8d9cad
JhonnM62 <jonnymejia62@gmail.com>
2023-04-15 14:30:01 -0500

    Union de la api con el login NOTA: SE QUITO EL ENCRYPT DE PASSWORD PARA QUE ESTA PASSWORD SEA LA DE LA FIRMA y el token

--- network/routes.js
+++ network/routes.js
@@ -3,10 +3,11 @@ const express = require('express')
 const bill    = require('../components/bill/network')
 const usersRoutes = require( "../components/login/src/routes/user.routes");
 const authRoutes =require( "../components/login/src/routes/auth.routes")
+const verifyToken = require("../components/login/src/middlewares/authJwt");
 
 //Logic
 const routes = server =>{
-    server.use('/xmlGenereteBill',bill)
+    server.use('/xmlGenereteBill',verifyToken, bill)
      // Routes login
     server.use("/api/users", usersRoutes);
     server.use("/api/auth", authRoutes);
