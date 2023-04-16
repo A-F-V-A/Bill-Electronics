79681b47de8275734a0d0b6b1537c692b6476473
JhonnM62 <jonnymejia62@gmail.com>
2023-04-16 14:51:20 -0500

    modicicacion del modelo y auth.controller, la funcion devuelve el id de igual forma la linea 38 la puede aplicar con lo que desee solo es exportar la SECRET de la linea 3, se agrego al modelo el serial

--- components/login/src/middlewares/authJwt.js
+++ components/login/src/middlewares/authJwt.js
@@ -3,22 +3,29 @@ const { SECRET }= require( "../config.js");
 const User= require( "../models/User.js");
 
 
+
 const verifyToken = async (req, res, next) => {
+  let password, serial, p12File;
   let token = req.headers["x-access-token"];
 
   if (!token) return res.status(403).json({ message: "No token provided" });
 
   try {
-    const decoded = jwt.verify(token, SECRET);
-    req.userId = decoded.id;
+  const decoded = jwt.verify(token, SECRET);
+  req.userId = decoded.id;
 
-    const user = await User.findById(req.userId, { password: 0 });
-    if (!user) return res.status().json({ message: "No user found" });
+  const user = await User.findById(req.userId, { password: 0 });
+  if (!user) return res.status(404).json({ message: "No user found" });
   
-    next();
+  ({ password, serial, p12File } = user);
+  console.log('>>>>>>>>>>>', serial, password );
+  next();
   } catch (error) {
     return res.status(401).json({ message: "Unauthorized!" });
   }
 };
 
-module.exports = verifyToken;
+module.exports = verifyToken
+
+
+
